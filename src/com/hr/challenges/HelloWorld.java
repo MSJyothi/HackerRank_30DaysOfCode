fc3a8a9f284cb5b24d2221d262f9a67e9b2861f7
MSJyothi <jyothi.msoman@gmail.com>
2020-10-12 18:08:44 -0400

    Day 0

---
+++ src/com/hr/challenges/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.hr.challenges;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+
+		// Create a Scanner object to read input from stdin.
+		Scanner scan = new Scanner(System.in);
+		
+		// Read a full line of input from stdin and save it to our variable, inputString.
+		String input = scan.nextLine();
+		
+		// Close the scanner object, because we've finished reading 
+        // all of the input from stdin needed for this challenge.
+		scan.close();
+		
+		// Print a string literal saying "Hello, World." to stdout.
+		System.out.println("Hello World");
+		
+		//prints the contents of inputString to stdout.
+		System.out.println("Input recieved" + input);
+
+	}
+
+}
