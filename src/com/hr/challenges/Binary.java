ac15fdc00ff210448a01454ae041270bfb258e86
MSJyothi <jyothi.msoman@gmail.com>
2020-10-18 21:20:03 -0400

    Day 10

--- src/com/hr/challenges/Binary.java
+++ src/com/hr/challenges/Binary.java
@@ -10,9 +10,12 @@ public class Binary {
 	public static void main(String[] args) {
 		int n = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		
+		
 
 		int count = 0;
 		int max = 0;
+		System.out.println(Integer.toBinaryString(n));
 		while (n > 0) {
 			int reminder = n % 2;
 			if (reminder == 0) {
