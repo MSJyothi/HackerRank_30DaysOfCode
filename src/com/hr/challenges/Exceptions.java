401a557a1ee4b2443e52bc948b1cb130b1b0a28e
MSJyothi <jyothi.msoman@gmail.com>
2020-11-10 09:04:51 -0500

    Day 16

---
+++ src/com/hr/challenges/Exceptions.java
@@ -0,0 +1,19 @@
+package com.hr.challenges;
+
+import java.util.*;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+
+		try {
+			int number = Integer.parseInt(S);
+			System.out.println(number);
+		} catch (Exception ex) {
+			System.out.println("Bad String");
+		}
+	}
+
+}
